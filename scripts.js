fa56e8fff74a54b800a0d1e29cc1a2cbfb45ef24
Jean Louis Salbego <jlsalbego@protonmail.com>
2019-02-24 22:06:09 +0000

    panic.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
+const DB_NAME = 'PHANTOM_BOOKMARKS_00';
+const OBJECT_STORE_NAME = 'bookmarks';
 const links = [];
+let DB;
 let currentPage = 0;
 
 /**
@@ -6,9 +9,73 @@ let currentPage = 0;
  */
 function setup() {
   console.log('setup');
+  initializeDatabase();
   document.querySelector('input#url').addEventListener('keyup', validateURL);
   document.querySelector('.submit-link-form > form').addEventListener('submit', addLink);
-  loadPersistedBookmarks();
+}
+
+function read() {
+  const transaction = DB.transaction(['bookmark']);
+  const objectStore = transaction.objectStore(OBJECT_STORE_NAME);
+  const request = objectStore.get(1);
+
+  request.onerror = event => {
+    console.log('Transaction failed');
+  };
+
+  request.onsuccess = event => {
+    if (request.result) {
+      console.log(request.result);
+    } else {
+      console.log('No data record');
+    }
+  };
+}
+
+/**
+ * open the database
+ */
+function initializeDatabase() {
+  // https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
+  window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+  window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
+  window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+  if (!window.indexedDB) {
+    alert("I'm sorry, your browser doesn't support a stable version of IndexedDB. This application won't work 😟");
+  }
+
+  const request = window.indexedDB.open(DB_NAME, 1);
+  request.onerror = event => {
+    console.error(event);
+    alert('Something went wrong while loading the database. Please reload.');
+  };
+  request.onsuccess = event => {
+    console.info(event);
+    DB = request.result;
+    console.info('Database opened successfully');
+    let objectStore;
+    if (!DB.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+      objectStore = DB.createObjectStore(OBJECT_STORE_NAME, {autoIncrement: true});
+      objectStore.createIndex('url', 'url', {unique: false});
+    }
+    read();
+  };
+  request.onupgradeneeded = event => {
+    console.info(event);
+    DB = event.target.result;
+    console.info('Database opened through `onupgradeneeded`');
+
+    let objectStore;
+    if (!DB.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+      objectStore = DB.createObjectStore(OBJECT_STORE_NAME, {autoIncrement: true});
+      objectStore.createIndex('url', 'url', {unique: false});
+      objectStore.transaction.oncomplete = event => {
+        const bookmarkObjectStore = DB.transaction(OBJECT_STORE_NAME, 'readwrite').objectStore(OBJECT_STORE_NAME)
+      }
+    }
+    // loadPersistedBookmarks();
+    // read();
+  };
 }
 
 /**
@@ -16,6 +83,8 @@ function setup() {
  */
 function loadPersistedBookmarks() {
   console.log('loading bookmarks');
+
+  // read();
   renderLinks();
   renderPagination();
 }
@@ -69,40 +138,58 @@ async function addLink(event) {
   console.log(url);
 
   links.push(url);
+
+  const request = DB.transaction(['bookmark'], 'readwrite')
+    .objectStore(OBJECT_STORE_NAME)
+    .add({url: 'https://phantom.land'});
+
+  request.onsuccess = event => {
+    console.info('The data has been written successfully');
+  };
+
+  request.onerror = event => {
+    console.warning('The data has been written failed');
+  };
+
   // TODO: display confirmation page
   renderLinks();
   return false;
 }
 
 // TODO: editLink (replace with input field)
-function editLink() {}
+function editLink() {
+}
 
 // TODO: updateLink (update in list)
-function updateLink() {}
+function updateLink() {
+}
 
 // TODO: deleteLink (delete from list)
-function deleteLink() {}
+function deleteLink() {
+}
 
 // TODO: include pagination
 // render a bookmark page
 function renderLinks() {
-  const urls = [
-    'https://phantom.land',
-    'https://google.com',
-    'https://twitter.com',
-    'https://facebook.com',
+  const links = [
+    {url: 'https://phantom.land', key: 0},
+    {url: 'https://google.com', key: 1},
+    {url: 'https://twitter.com', key: 2},
+    {url: 'https://facebook.com', key: 3}
   ];
 
   const bookmarkList = document.querySelector('.bookmark-list > ul');
 
-  [...urls, ...urls, ...urls, ...urls, ...urls, ...urls, ...urls, ...urls]
-    .forEach(url =>{
+  [...links, ...links, ...links, ...links, ...links, ...links, ...links, ...links]
+    .forEach(link => {
       const listElement = document.createElement('li');
       const anchor = document.createElement('a');
-      const text = document.createTextNode(url);
+      const text = document.createTextNode(link.url);
 
-      anchor.title = url;
-      anchor.href = url;
+      anchor.title = link.url;
+      anchor.href = link.url;
+      anchor.setAttribute('target', '_blank');
+      anchor.setAttribute('data-key', 1);
       anchor.appendChild(text); // text into <a>
 
       listElement.classList.add('bookmark');
@@ -114,4 +201,5 @@ function renderLinks() {
 }
 
 //TODO: render new pagination
-function renderPagination() {}
+function renderPagination() {
+}
