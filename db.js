5eff281533b7adec1fb3674be3419d8ab2fcbc30
Jean Louis Salbego <jlsalbego@protonmail.com>
2019-02-25 19:46:28 +0000

    added function to populate indexedDB

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const OBJECT_STORE_NAME = 'Bookmarks';
  * initialize the database and object store
  * @callback cb
  */
-function initializeDatabase(cb = () => {}) {
+function initializeDatabase(cb = () => {
+}) {
   // https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
   window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
   window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {READ_WRITE: "readwrite"}; // This line should only be needed if it is needed to support the object's constants for older browsers
@@ -22,6 +23,7 @@ function initializeDatabase(cb = () => {}) {
 
   request.onsuccess = function (event) {
     console.info(event);
+    // populate();
     cb();
   };
 
@@ -32,7 +34,27 @@ function initializeDatabase(cb = () => {}) {
     const DB = request.result;
     const store = DB.createObjectStore(OBJECT_STORE_NAME, {autoIncrement: true});
     store.createIndex('name', 'name', {unique: false});
+    // populate();
     cb();
   };
 }
 
+/**
+ * add fake bookmarks for testing purpose
+ */
+function populate() {
+  const bookmarks = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'.split('').map((letter) => ({
+    name: letter,
+    url: `https://google.com/${letter}`,
+  }));
+  const request = window.indexedDB.open(DB_NAME, 1);
+  request.onerror = console.error;
+  request.onsuccess = () => {
+    const db = request.result;
+    const transaction = db.transaction(OBJECT_STORE_NAME, 'readwrite');
+    const store = transaction.objectStore(OBJECT_STORE_NAME);
+    db.onerror = console.error;
+    transaction.oncomplete = db.close;
+    bookmarks.forEach((b) => store.put(b));
+  };
+}
